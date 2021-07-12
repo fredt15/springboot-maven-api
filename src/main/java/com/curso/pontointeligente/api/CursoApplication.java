0e855c5db16c0b4f0e310f0b94e00d6dd02c932a
Fred <fredtbh15@gmail.com>
2021-07-12 18:30:15 -0300

    Funcionando após concertar o bug do flyway

--- src/main/java/com/curso/pontointeligente/api/CursoApplication.java
+++ src/main/java/com/curso/pontointeligente/api/CursoApplication.java
@@ -1,4 +1,4 @@
-package com.curso.api;
+package com.curso.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/curso/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,8 @@
+package com.curso.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+
+    ROLE_ADMOIN,
+    ROLE_USUARIO;
+
+}
